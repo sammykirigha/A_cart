8ae1b98e0a94b1ff9d64575144006d135f37f2fc
sammykirigha <dkirigha18@gmail.com>
2021-08-14 10:08:58 +0300

    added cart ui

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+
+
+function ratingFunction(rating) {
+    return Array(rating).fill().map(_, i) => {
+        <p>&#9733</p>
+    }
+}
+
+document.getElementById("rating").innerHTML = ratingFunction(4)
