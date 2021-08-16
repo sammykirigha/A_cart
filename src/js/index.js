3effc506872139b3a45184b3c5a7640fed42eed6
sammykirigha <dkirigha18@gmail.com>
2021-08-16 09:56:34 +0300

    added a pure html, css and javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 
 
 function ratingFunction(rating) {
-    return Array(rating).fill().map(_, i) => {
-        <p>&#9733</p>
-    }
+    return Array(rating).fill().map(_, i) => (
+        `<p>&#9733</p>`
+    )
 }
 
 document.getElementById("rating").innerHTML = ratingFunction(4)
+
